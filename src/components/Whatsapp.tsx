b45dea1e0329d26dc06f8c9801d3d9f5325a70f4
AndHak <andresfg13789@gmail.com>
2025-07-22 00:56:34 -0500

    whatsapp ajustado

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -1,23 +1,21 @@
-
-import { Button } from "./ui/button"
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 
 export function WhatsAppStickyButton() {
   const whatsappLink =
-    "https://wa.me/3128758461?text=Hola%2C%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20sus%20productos%20crochet."
+    "https://wa.me/3128758461?text=Hola%2C%20me%20gustar%C3%ADa%20saber%20m%C3%A1s%20sobre%20sus%20productos%20crochet.";
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
-        <Button
-          size="icon"
-          className="w-16 h-16 rounded-full bg-green-500 hover:bg-green-600 shadow-lg transition-transform transform hover:scale-110"
-          aria-label="Contactar por WhatsApp"
-        >
-          <Icon icon="logos:whatsapp-icon" className="h-full w-full text-white" />
-          <span className="sr-only">Contactar por WhatsApp</span>
-        </Button>
+      <a
+        href={whatsappLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center justify-center w-16 h-16 bg-green-500 rounded-full hover:bg-green-600 transition-transform transform hover:scale-110 shadow-lg"
+        aria-label="Contactar por WhatsApp"
+      >
+        <Icon icon="mdi:whatsapp" className="text-white p-2" width={48} height={48}/>
+        <span className="sr-only">Contactar por WhatsApp</span>
       </a>
     </div>
-  )
+  );
 }
